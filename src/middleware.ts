8c1a57ba0924eaf37085e0275986197b2ec24b19
andurila19-lgtm <andurila19@gmail.com>
2026-03-01 08:22:43 +0700

    fix: restore standard middleware.ts format

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,7 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-const handleIntl = createMiddleware(routing);
-
-export default function proxy(request: any) {
-  return handleIntl(request);
-}
+export default createMiddleware(routing);
 
 export const config = {
   // Match only internationalized pathnames
